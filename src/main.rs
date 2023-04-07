68f0415e30642beacc1b10e0a11ba01600d36505
Ken Micklas <git@kmicklas.com>
2023-04-07 10:07:55 +0100

    Start CLI parser

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use clap::Parser;
+use std::path::PathBuf;
+
 mod keep;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(help = "Google Keep directory, Takeout/Keep in extracted Takeout archive")]
+    keep_dir: PathBuf,
+
+    #[arg(short, long, default_value = ".")]
+    output_dir: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{:?}", args)
 }
