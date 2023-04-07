4009b3b4a49bd4eed37fd587600af6d020e11daf
Ken Micklas <git@kmicklas.com>
2023-04-07 14:42:06 +0100

    Remove Unicode text which confuses rustfmt

--- src/keep.rs
+++ src/keep.rs
@@ -37,7 +37,7 @@ mod tests {
 
     #[test]
     fn test_deserialize() {
-        let data = r#"{"color":"DEFAULT","isTrashed":false,"isPinned":false,"isArchived":false,"textContent":"( ͡° ͜ʖ ͡°)\n( ͠° ͟ʖ ͡°)\n(ง\u2019̀-\u2019́)ง","title":"Emojis","userEditedTimestampUsec":1441394812887000,"createdTimestampUsec":1412018652099000,"labels":[{"name":"Reference"}]}"#;
+        let data = r#"{"color":"DEFAULT","isTrashed":false,"isPinned":false,"isArchived":false,"textContent":"content","title":"title","userEditedTimestampUsec":1441394812887000,"createdTimestampUsec":1412018652099000,"labels":[{"name":"Reference"}]}"#;
 
         let note: Note = serde_json::from_str(data).unwrap();
 
@@ -46,8 +46,8 @@ mod tests {
             Note {
                 is_trashed: false,
                 is_archived: false,
-                text_content: "( \u{361}° \u{35c}ʖ \u{361}°)\n( \u{360}° \u{35f}ʖ \u{361}°)\n(ง’\u{300}-’\u{301})ง".to_owned(),
-                title: "Emojis".to_owned(),
+                text_content: "content".to_owned(),
+                title: "title".to_owned(),
                 user_edited_timestamp_usec: 1441394812887000,
                 created_timestamp_usec: 1412018652099000,
             }
